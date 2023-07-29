5c5a37dbff6f945aa2e560b6e6889430e41133a2
Aykut Kav <ayktkav@gmail.com>
2023-07-29 22:59:56 +0200

    done

--- server/routes/stripe.routes.js
+++ server/routes/stripe.routes.js
@@ -31,57 +31,57 @@ router.post(
     });
 
     const session = await stripe.checkout.sessions.create({
-      // payment_method_types: ["card"],
-      // shipping_address_collection: {
-      //   allowed_countries: ["US", "CA", "TR", "DE"],
-      // },
-      // shipping_options: [
-      //   {
-      //     shipping_rate_data: {
-      //       type: "fixed_amount",
-      //       fixed_amount: {
-      //         amount: 0,
-      //         currency: "usd",
-      //       },
-      //       display_name: "Free shipping",
-      //       // Delivers between 5-7 business days
-      //       delivery_estimate: {
-      //         minimum: {
-      //           unit: "business_day",
-      //           value: 5,
-      //         },
-      //         maximum: {
-      //           unit: "business_day",
-      //           value: 7,
-      //         },
-      //       },
-      //     },
-      //   },
-      //   {
-      //     shipping_rate_data: {
-      //       type: "fixed_amount",
-      //       fixed_amount: {
-      //         amount: 1500,
-      //         currency: "usd",
-      //       },
-      //       display_name: "Next day air",
-      // Delivers in exactly 1 business day
-      //       delivery_estimate: {
-      //         minimum: {
-      //           unit: "business_day",
-      //           value: 1,
-      //         },
-      //         maximum: {
-      //           unit: "business_day",
-      //           value: 1,
-      //         },
-      //       },
-      //     },
-      //   },
-      // ],
-      // phone_number_collection: {
-      //   enabled: true,
-      // },
+      payment_method_types: ["card"],
+      shipping_address_collection: {
+        allowed_countries: ["US", "CA", "TR", "DE"],
+      },
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: {
+              amount: 0,
+              currency: "usd",
+            },
+            display_name: "Free shipping",
+            // Delivers between 5-7 business days
+            delivery_estimate: {
+              minimum: {
+                unit: "business_day",
+                value: 5,
+              },
+              maximum: {
+                unit: "business_day",
+                value: 7,
+              },
+            },
+          },
+        },
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: {
+              amount: 1500,
+              currency: "usd",
+            },
+            display_name: "Next day air",
+            // Delivers in exactly 1 business day
+            delivery_estimate: {
+              minimum: {
+                unit: "business_day",
+                value: 1,
+              },
+              maximum: {
+                unit: "business_day",
+                value: 1,
+              },
+            },
+          },
+        },
+      ],
+      phone_number_collection: {
+        enabled: true,
+      },
       line_items,
       mode: "payment",
       // this can change on deploy version
