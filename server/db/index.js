4c60170d69a2d64b3bd7b649f9297b98b1ce500a
kiwiscode <ayktkav@gmail.com>
2025-03-15 01:14:01 +0100

    when adding an artwork, artist whose names match the creators are found, and t he artist's ID is assigned to the artwork's artist field

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,46 @@
 const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
+const Artwork = require("../models/Artwork.model");
+const Artist = require("../models/Artist.model");
 
 const MONGO_URI = process.env.MONGODB_URI;
 
 mongoose
   .connect(MONGO_URI)
-  .then((x) => {
+  .then(async (x) => {
     const databaseName = x.connections[0].name;
     console.log("connected to the database:", databaseName);
+
+    // const artworkData = JSON.parse(
+    //   fs.readFileSync(
+    //     path.join(__dirname, "../../client/src/data/dataArtwork.json"),
+    //     "utf8"
+    //   )
+    // );
+
+    // console.log("artwork data:", artworkData);
+
+    // for (const artwork of artworkData) {
+    //   try {
+    //     const artist = await Artist.findOne({ name: artwork.creator });
+
+    //     if (!artist) {
+    //       console.log(`Artist with name "${artwork.creator}" not found.`);
+    //       continue;
+    //     }
+
+    //     artwork.artist = artist._id;
+
+    //     const newArtwork = new Artwork(artwork);
+    //     await newArtwork.save();
+    //     console.log(
+    //       `Artwork "${artwork.title}" by artist "${artist.name}" successfully added to the database.`
+    //     );
+    //   } catch (err) {
+    //     console.error(`Error processing artwork "${artwork.title}": `, err);
+    //   }
+    // }
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
